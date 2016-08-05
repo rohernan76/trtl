e632031930b9ddab92e5ed01f6338749211d0a5b
KellyKapp <kellykapp85@gmail.com>
2016-08-05 10:15:04 -0600

    Friday morning updates

--- Public/index.js
+++ Public/index.js
@@ -12,8 +12,8 @@ function Snippet (id, text, answer, category, difficulty) {
 
 // sample population of the objects so we can build out the front end
 // jjf: 8/4/16
-snippetArr[0] = new Snippet(1, "css text here", "css", "languages", 1);
-snippetArr[1] = new Snippet(2, "html text here", "html", "languages", 1);
+snippetArr[0] = new Snippet(1, "body { background-color: blue}", "css", "languages", 1);
+snippetArr[1] = new Snippet(2, "\<!DOCTYPE html\>", "html", "languages", 1);
 snippetArr[2] = new Snippet(3, "js text here", "js", "languages", 1);
 snippetArr[3] = new Snippet(4, "more css", "css", "languages", 1);
 snippetArr[4] = new Snippet(5, "more html", "html", "languages", 1);
@@ -23,13 +23,36 @@ snippetArr[5] = new Snippet(6, "more js", "js", "languages", 1);
 // just for test purposes to make sure array and objects work as expected
 // jjf 8/4/16
 $(document).ready(function() {
-	$('#snippetBox1').html(snippetArr[0].text);
-	$('#snippetBox2').html(snippetArr[1].text);
-	$('#snippetBox3').html(snippetArr[2].text);
+	$('#snippetBox').html(snippetArr[0].text);
 });
 
 
+//JS for Modal
+// Get the modal
+var modal = document.getElementById('tutorialModal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 
 
