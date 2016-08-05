57eff3923a927928f08f06bc16bde00077a3779d
joshuaforman <joshuaforman@Joshuas-MacBook-Air.local>
2016-08-05 13:37:40 -0600

    cleaned up code for running through the snippet array

--- Public/index.js
+++ Public/index.js
@@ -2,30 +2,23 @@ var snippetArr = [];
 
 // this is what our object will look like in Mongo. Creating it here for now so we can build the front end first.
 // jjf: 8/4/16
-function Snippet (id, text, answer, category, difficulty) {
+function Snippet (id, text, answer, answerId, category, difficulty) {
 	this.id = id;
 	this.text = text;
 	this.answer = answer;
+	this.answerId = answerId;
 	this.category = category;
 	this.difficulty = difficulty;
 }
 
 // sample population of the objects so we can build out the front end
 // jjf: 8/4/16
-snippetArr[0] = new Snippet(1, "body { background-color: blue}", "css", "languages", 1);
-snippetArr[1] = new Snippet(2, "\<!DOCTYPE html\>", "html", "languages", 1);
-snippetArr[2] = new Snippet(3, "js text here", "js", "languages", 1);
-snippetArr[3] = new Snippet(4, "more css", "css", "languages", 1);
-snippetArr[4] = new Snippet(5, "more html", "html", "languages", 1);
-snippetArr[5] = new Snippet(6, "more js", "js", "languages", 1);
-
-
-// just for test purposes to make sure array and objects work as expected
-// jjf 8/4/16
-$(document).ready(function() {
-	$('#snippetBox').html(snippetArr[0].text);
-});
-
+snippetArr[0] = new Snippet(1, "body { background-color: blue}", "css", 2, "languages", 1);
+snippetArr[1] = new Snippet(2, "html", "html", 1, "languages", 1);
+snippetArr[2] = new Snippet(3, "js text here", "js", 3, "languages", 1);
+snippetArr[3] = new Snippet(4, "more css", "css", 2, "languages", 1);
+snippetArr[4] = new Snippet(5, "more html", "html", 1, "languages", 1);
+snippetArr[5] = new Snippet(6, "more js", "js", 3, "languages", 1);
 
 //JS for Modal
 // Get the modal
@@ -54,7 +47,45 @@ window.onclick = function(event) {
     }
 };
 
+/////////////////// Code for testing answers and tracking through snippet array
 
+var snipNum = 0;
+var firstTryScore = 0;
+var firstTry = true;
+
+// start with the first snippet
+$(document).ready(function() {
+	//$('#snippetBox').html(snippetArr[0].text);
+	showNewSnippet(0);
+});
+
+// listen for click events on the buckets. When fired, check if it is the right answer. For now, only console.logging the event. 
+$(".bucket").click(function(evt) {
+	console.log("bucket clicked", evt.target.id);
+	var answerId = evt.target.id[6];
+	if (answerId == snippetArr[snipNum].answerId) {
+		console.log("Answer is correct", snippetArr[snipNum].answerId, answerId);
+		// if it's the right answer, increment snipNum and then show new Snippet
+		snipNum++;
+		if (firstTry) {
+			firstTryScore++;
+		}
+		if (snippetArr[snipNum]) {
+			showNewSnippet(snipNum);
+		} else {
+			alert("Game is Over. You got " + firstTryScore + " right on the first try.");
+		}
+		firstTry = true;
+	} else {
+		console.log("Answer is not correct", snippetArr[snipNum].answerId, answerId);
+		firstTry = false;
+	}
+});
+
+function showNewSnippet(snipNum) {
+	// code for dragging the element will do in here, 
+	$('#snippetBox').html(snippetArr[snipNum].text);
+}
 
 
 //	$('#snippetBox3').html("Id:" + snippetArr[2].id + "<br>Question Text: " + snippetArr[2].text + "<br>Answer: "+ snippetArr[2].answer + "<br>Category: " + snippetArr[2].category);
